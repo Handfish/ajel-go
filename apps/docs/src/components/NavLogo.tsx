0cd0985c1c59bc317a0848c17b5fae04578f9d06
Kenneth Udovic <kennyudovic@gmail.com>
2024-02-04 11:42:33 -0500

    chore: logo in docs

--- apps/docs/src/components/NavLogo.tsx
+++ apps/docs/src/components/NavLogo.tsx
@@ -15,6 +15,11 @@ export default function NavLogo() {
         style={{ height: "calc(var(--nextra-navbar-height) - 35px)" }}
         alt="ajel logo"
       />
+      <img
+        src="https://go.dev/blog/go-brand/Go-Logo/SVG/Go-Logo_Blue.svg"
+        style={{ height: "calc(var(--nextra-navbar-height) - 35px)" }}
+        alt="ajel logo"
+      />
       ajel-go
     </div>
   );
